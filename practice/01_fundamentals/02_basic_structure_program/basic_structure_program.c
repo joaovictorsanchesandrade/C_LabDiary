838bc22a82f7339273d370a7b1d680b97ce19b6c
João Victor Sanches Andrade <joaovictorsanchesandradeestuda@gmail.com>
2025-08-27 19:09:36 -0300

    feat: Estrutura basica de um programa C

---
+++ practice/01_fundamentals/02_basic_structure_program/basic_structure_program.c
@@ -0,0 +1,13 @@
+/*
+    Estrutura básica de um programa em C
+*/
+
+// Todo programa em C é obrigatório ter a função main.
+// É a partir dela que o compilador inicia a execução do programa.
+int main(){ // As funções em C possuem tipos de retorno definidos.
+            // Abordaremos esse conceito com mais detalhes posteriormente.
+    // Exemplo de uma soma simples
+    int a = 20;
+    int b = 20;
+    return a + b;
+}
